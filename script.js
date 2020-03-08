9e32b71d089f0849cb164b982472a1e2996af701
Mohammed Yaseen <mailofyaseen@gmail.com>
2020-03-08 17:50:40 +0000

    Add game functionality

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(humanGuess, computerGuess, target) {
+  humanDifference = Math.abs(target - humanGuess);
+  computerDifference = Math.abs(target - computerGuess);
+  return humanDifference <= computerDifference;
+}
+
+function updateScore(winner) {
+  winner === 'human' ? humanScore++ : computerScore++;
+}
+
+function advanceRound() {
+  currentRoundNumber++;
+}
